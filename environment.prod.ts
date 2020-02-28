6ecdbf9b6ba402efa64766d6befc597edd69e68c
Damien Trouillet <contact@damien-trouillet.fr>
2020-02-28 13:00:34 +0100

    customize k8s deployments

---
+++ environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  apiUrl: '/ebad',
+  jwt: true,
+  atHash: 'at_hash',
+  issuer: '',
+  clientId: 'ebad',
+  loginUrl: '',
+  scope: 'openid profile',
+  logoutUrl: '',
+  userinfoEndpoint: ''
+};
